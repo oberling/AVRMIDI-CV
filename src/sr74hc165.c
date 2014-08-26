e00c3e622c30c0afca5259e4a6534e50b18be21c
Stephan Bergemann <oberling@deroberling.de>
2014-08-26 08:09:48 +0200

    changed PIN orientation of CE_PIN

--- src/sr74hc165.c
+++ src/sr74hc165.c
@@ -5,7 +5,7 @@ void sr74hc165_init(uint8_t num_modules) {
 	init_spi();
 	SR_PL_CE_DDR |= (1<<SR_CE_PIN)|(1<<SR_PL_PIN); // set as output
 	SR_PL_CE_PORT |= (1<<SR_PL_PIN);
-	SR_PL_CE_PORT &= ~(1<<SR_CE_PIN);
+	SR_PL_CE_PORT |= (1<<SR_CE_PIN);
 }
 
 void sr74hc165_read(unsigned char* output_buffer, uint8_t num_modules) {
@@ -15,12 +15,12 @@ void sr74hc165_read(unsigned char* output_buffer, uint8_t num_modules) {
 	_delay_us(1);
 	SR_PL_CE_PORT |= (1<<SR_PL_PIN);
 	// chip select
-	SR_PL_CE_PORT |= (1<<SR_CE_PIN);
+	SR_PL_CE_PORT &= ~(1<<SR_CE_PIN);
 	// now shift our data into the output_buffer
 	uint8_t i=0;
 	for(; i<num_modules; i++) {
 		*(output_buffer+i) = spi_transfer(0x00);
 	}
-	SR_PL_CE_PORT &= ~(1<<SR_CE_PIN);
+	SR_PL_CE_PORT |= (1<<SR_CE_PIN);
 }
 
