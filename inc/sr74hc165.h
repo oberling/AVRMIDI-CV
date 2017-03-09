8df8e81496f76388da2e4248a5ab53b21f53c9ee
Stephan Bergemann <oberling@deroberling.de>
2017-03-09 16:32:08 +0100

    some clarifying comments

--- inc/sr74hc165.h
+++ inc/sr74hc165.h
@@ -3,14 +3,19 @@
 #include <stdint.h>
 #include "spi.h"
 
+// chip enable pin
 #ifndef SR_CE_PIN
 #pragma message "please define SR_CE_PIN - defaulting to PD6"
 #define SR_CE_PIN	PD6
 #endif
+
+// parallel load pin
 #ifndef SR_PL_PIN
 #pragma message "please define SR_PL_PIN - defaulting to PD7"
 #define SR_PL_PIN	PD7
 #endif
+
+
 #ifndef SR_PL_CE_PORT
 #pragma message "SR_PL_CE_PORT not defined - defaulting to PORTD"
 #define SR_PL_CE_PORT	PORTD
