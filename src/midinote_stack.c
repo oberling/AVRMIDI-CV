f91511066bff2f1c13d8bec6bf0b0f0ae20a9229
Stephan Bergemann <oberling@deroberling.de>
2017-03-15 23:03:10 +0100

    added some comments and more robust if-clause

--- src/midinote_stack.c
+++ src/midinote_stack.c
@@ -9,20 +9,22 @@ bool midinote_stack_init(midinote_stack_t* s) {
 
 bool midinote_stack_push(midinote_stack_t* s, midinote_t d) {
 	uint8_t i=0;
+	// search for note and update it if found
 	for(;i<s->position;i++) {
 		if(s->data[i].note == d.note) {
 			s->data[i] = d;
 			return true;
 		}
 	}
-	// if no update of existing note i is now at s->position
-	// if stack is full: forget oldest note
-	if(s->position==MIDINOTE_STACK_SIZE) {
+	// if no update of existing note then i is now at s->position
+	// if stack is full: forget oldest note - shift all notes one position to front
+	if(s->position>=MIDINOTE_STACK_SIZE) {
 		for(i=0;i<MIDINOTE_STACK_SIZE-1; i++) {
 			s->data[i] = s->data[i+1];
 		}
-		s->position = i;
+		s->position--; // decrease position only to increase it again :)
 	}
+	// finally put in new note and increase position pointer
 	s->data[s->position++] = d;
 	return true;
 }
@@ -50,16 +52,20 @@ bool midinote_stack_remove(midinote_stack_t* s, note_t remnote) {
 }
 
 bool midinote_stack_peek_n(midinote_stack_t* s, uint8_t num_req, midinote_t** first, uint8_t* num_ret) {
+	// if no notes where requested or the stack is empty return nothing and false
 	if(num_req == 0 || s->position == 0) {
 		*first = NULL;
 		*num_ret = 0;
 		return false;
 	}
+	// if more or equal notes are requested than are on the stack 
+	// return all notes from stack and true
 	if(num_req >= s->position) {
 		*first = s->data;
 		*num_ret = s->position;
 		return true;
 	}
+	// otherwise return as many as requested taking the most recent added ones
 	*first = (s->data)+(s->position)-num_req;
 	*num_ret = num_req;
 	return true;
