f795956b9689682a1e02e9beebe9833a512ce703
Stephan Bergemann <oberling@deroberling.de>
2017-03-19 23:00:18 +0100

    complete rewrite of midibuffer serial input handler

--- src/midibuffer.c
+++ src/midibuffer.c
@@ -37,125 +37,103 @@ bool __handle_realtime_byte_message(midibuffer_t* b, midimessage_t* m, uint8_t b
 }
 
 bool midibuffer_get(midibuffer_t* b, midimessage_t* m) {
-	if(ringbuffer_empty(&(b->buffer)))
-		return false;
 	unsigned char byte = 0x00;
 
 	//
-	// midi-definitions from https://www.midi.org/specifications/item/table-1-summary-of-midi-message
+	// MIDI Message specifications:
+	// https://www.midi.org/specifications/item/table-1-summary-of-midi-message
 	//
 
-	// handle sysex-messages
-	do {
-		// peek byte and check if we have a sysex-message here
-		if(!ringbuffer_peek(&(b->buffer), &byte)) {
-			return false;
-		}
-		// the following saves us 4 Byte Flash Space compared to a switch-case statement
+	while(!ringbuffer_empty(&(b->buffer))) {
+		// handle message byte by byte - peek byte and dispatch it
+		ringbuffer_get(&(b->buffer), &byte);
+
+		// sysex handling first - discard all sysex
 		if(byte == SYSEX_BEGIN) {
 			midibuffer_issysex = true;
+			continue; // discard byte
 		} else if (byte == SYSEX_END) {
 			midibuffer_issysex = false;
-			ringbuffer_get(&(b->buffer), &byte); // take away last sysex byte
-			// read next byte if possible for further message handling
-			if(!ringbuffer_peek(&(b->buffer), &byte)) {
-				return false;
-			} else if (byte == SYSEX_BEGIN) { // yet another sysex message following 
-				midibuffer_issysex = true;
+			continue; // discard byte
+		}
+
+		if(midibuffer_issysex) {
+			continue; // discard byte
+		} 
+
+		// no more sysex handling from here on
+		if (byte >= CLOCK_SIGNAL) { // realtime message
+			// dispatch the following...
+			if(
+				byte == CLOCK_SIGNAL || 
+				byte == CLOCK_START || 
+				byte == CLOCK_CONTINUE || 
+				byte == CLOCK_STOP ||
+				byte == 0xFE ||		// active sensing
+				byte == 0xFF		// reset
+			) {
+				m->byte[0] = byte;
+				return true;
+			} else if( // ... and discard these
+				byte == 0xF9 ||		// undefined - reserved
+				byte == 0xFD		// undefined - reserved
+			) {
+				continue; // discard;
 			}
-		}
-		// handle real-time messages
-		if(__is_realtime_byte_message(byte)) {
-			return __handle_realtime_byte_message(b, m, byte);
-		}
-		// step out if we are not or no longer in a sysex message
-		if(!midibuffer_issysex)
-			break;
-		// discard sysex bytes
-		ringbuffer_get(&(b->buffer), &byte); // discard byte
-	} while(!ringbuffer_empty(&(b->buffer)));
+		} else if (byte >= 0xF4) { // other one-byte messages
+			// including 0xF4 (undefined), 0xF5 (undefined), 0xF6 (tune request)
+			// and 0xF7 (SYSEX_END) which is already handled above
+			continue; // discard
+		} else if (byte >= 0x80) { // status byte
+			current_message.byte[0] = byte;
+			current_message_next_fillbyte = 1;
+			continue;
+		} else { // data byte
+			// get messagetype without possible channel to be able to compare it
+			uint8_t current_status = current_message.byte[0];
+			if(current_status == 0) {
+				continue;
+			}
+			uint8_t status_nibble = current_status >> 4;
 
-	// handle one-byte messages
-	if(byte >= 0xF4) { // 0xF4 = first one-byte message
-		// handle any one-byte message...
-		if(__is_realtime_byte_message(byte)) {
-			return __handle_realtime_byte_message(b, m, byte);
-		}
-		// ... and discarding the following messages
-		if(	
-			byte == 0xF4 ||		// undefined - reserved
-			byte == 0xF5 ||		// undefined - reserved
-			byte == 0xF6 ||		// tune request
-			byte == SYSEX_END	// only for the sake of completeness (handled above)
-		  ) {
-			ringbuffer_get(&(b->buffer), &byte); // discard
-			return false;
-		}
-		// never reached
-	}
-
-	// if status byte
-	if(byte >= 0x80) {
-		// save status type (needed afterwards and for running status)
-		ringbuffer_get(&(b->buffer), current_message.byte);
-		current_message_next_fillbyte = 1;
-		// and try to get the first data byte
-		// (we already handled all one-byte messages above)
-		if(!ringbuffer_peek(&(b->buffer), &byte)) {
-			return false;
-		}
-	}
-	// byte must be the first data byte from here on
-
-	// get messagetype without possible channel to be able to compare it
-	uint8_t current_status = current_message.byte[0];
-	uint8_t status_nibble = current_status >> 4;
-
-	uint8_t num_expected_message_bytes = 0;
-	if( // 3-byte message
-		status_nibble == NOTE_OFF_NIBBLE ||
-		status_nibble == NOTE_ON_NIBBLE ||
-		status_nibble == 0xA || // polyphonic key pressure (polyphonic aftertouch)
-		status_nibble == 0xB || // CC
-		status_nibble == 0xE || // PitchBend
-		current_status == 0xF2  // Song Position Pointer
-	) {
-		num_expected_message_bytes = 3;
-//		return ringbuffer_getn_or_nothing(&(b->buffer), &(m->byte[1]), 2);
-	} else if ( // 2-byte message
-		status_nibble == 0xC ||   // program change
-		status_nibble == 0xD ||   // channel pressure (simple aftertouch)
-		current_status == 0xF1 || // MIDI Timecode Quarter Frame
-		current_status == 0xF3    // Song Select
-	) {
-		num_expected_message_bytes = 2;
-//		return ringbuffer_getn_or_nothing(&(b->buffer), &(m->byte[1]), 1);
-	} else {
-		ringbuffer_get(&(b->buffer), &byte); // discard
-		return false;
-	}
-
-	do {
-		if(!ringbuffer_peek(&(b->buffer), &byte)) {
-			return false;
-		}
-		if(__is_realtime_byte_message(byte)) {
-			return __handle_realtime_byte_message(b, m, byte);
-		} else {
-			ringbuffer_get(&(b->buffer), &(current_message.byte[current_message_next_fillbyte++]));
+			uint8_t num_expected_message_bytes = 0;
+			if( // 3-byte message
+				status_nibble == NOTE_OFF_NIBBLE ||
+				status_nibble == NOTE_ON_NIBBLE ||
+				status_nibble == 0xA || // polyphonic key pressure (polyphonic aftertouch)
+				status_nibble == 0xB || // CC
+				status_nibble == 0xE || // PitchBend
+				current_status == 0xF2  // Song Position Pointer
+			) {
+				num_expected_message_bytes = 3;
+			} else if ( // 2-byte message
+				status_nibble == 0xC ||   // program change
+				status_nibble == 0xD ||   // channel pressure (simple aftertouch)
+				current_status == 0xF1 || // MIDI Timecode Quarter Frame
+				current_status == 0xF3    // Song Select
+			) {
+				num_expected_message_bytes = 2;
+			}
+			current_message.byte[current_message_next_fillbyte++] = byte;
 			if(current_message_next_fillbyte == num_expected_message_bytes) {
 				while(current_message_next_fillbyte--) {
+					// copy all bytes to outgoing midi message
 					m->byte[current_message_next_fillbyte] = current_message.byte[current_message_next_fillbyte];
-					if(current_message_next_fillbyte>0) { // running status
+					if(current_message_next_fillbyte>0 || current_status >= 0xF0) { // running status
 						current_message.byte[current_message_next_fillbyte] = 0x00;
 					}
 				}
-				current_message_next_fillbyte = 1;
+				// preserve last status for running status
+				if(current_status >=0xF0) {
+					current_message_next_fillbyte = 0;
+				} else { // ... but only for 0x80-0xF0
+					current_message_next_fillbyte = 1;
+				}
 				return true;
 			}
+			continue;
 		}
-	} while (!ringbuffer_empty(&(b->buffer)));
-	// only maybe reached on boot/connect (current_status==0), when searching for first message
+	}
 	return false;
 }
 
